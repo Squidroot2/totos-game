13103222573ef376fe4eadfa929edefb15ba942f
Squidroot2 <hpfoley1@gmail.com>
2019-05-05 15:51:18 -0400

    Created Initial Pygame functionality

--- main.py
+++ main.py
@@ -1,1 +1,40 @@
+#third party modules
+import pygame, tcod, sys
+from pygame.constants import *
 
+#my modules
+from scripts.constants import *
+
+def initializePygame():
+    '''Initializes the pygame modules and creates the global variables SCREEN and FPS_CLOCK'''
+
+    global SCREEN, FPS_CLOCK
+    pygame.init()
+    FPS_CLOCK = pygame.time.Clock()
+    SCREEN = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+
+def runGameLoop():
+    '''runs the main game loop as long as the run_game boolean is true'''
+
+    run_game = True
+
+    #game loop
+    while run_game:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                run_game = False
+
+        pygame.display.update()
+        FPS_CLOCK.tick(FPS)
+
+def terminateGame():
+    '''Quits the game'''
+    pygame.quit()
+    sys.exit()
+
+
+
+if __name__ == '__main__':
+    initializePygame()
+    runGameLoop()
+    terminateGame()
---
+++ scripts/constants.py
@@ -0,0 +1,13 @@
+'''
+This module contains constants used throughout the grim ranger game
+'''
+FPS = 60
+
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+#                    R   G   B
+COLORS = {'BLACK': (  0,  0,  0),
+          'WHITE': (255,255,255)}
+
+BG_COLOR = COLORS['BLACK']
