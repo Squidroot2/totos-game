2d248fa68abc80bc585fa384de6397b9aca089fc
Squidroot2 <hayden.foley@gd-ms.com>
2019-07-09 08:47:02 -0400

    Added Weapon import from ini

--- scripts/items.py
+++ scripts/items.py
@@ -15,16 +15,22 @@ class Item(Entity):
 
 class Weapon(Item):
     
-    def __init__(self, name, level, location, x=None, y=None):
+    def __init__(self, id, location, x=None, y=None):
         super().__init__(location, x, y)
-        self.name = name
         config = _readINI
+        self.name =             config[id].get('name')
+        self.melee_damage =     config[id].getint('melee_damage')
+        self.is_quick_draw =    config[id].getboolean('quick_draw')
+        self.fire_rate =        config[id].getint('fire_rate')
+        self.difficulty =       config[id].getint('difficulty')
+        self.is_ranged =        config[id].getboolean('is_ranged')
+        if self.is_ranged:
+            self.ranged_damage = config[id].getint('ranged_damage')
+            self.energy_per_shot = config[id].getint('energy_per_shot')
+        
 
     def _readINI(self):
         config = configparser.ConfigParser()
         config.read(WEAPON_INI)
         return config
 
-
-
-    
