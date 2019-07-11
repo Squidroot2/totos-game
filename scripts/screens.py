0340ea7ad8db1be288185eb28131303205e90a75
hayden.foley <hpfoley1@gmail.com>
2019-07-11 19:50:11 -0400

    Added comments and syntactic sugar to titleScreen function

--- scripts/screens.py
+++ scripts/screens.py
@@ -6,10 +6,13 @@ from pygame.constants import *
 
 
 def titleScreen(window, fps_clock):
+    """Displays the Title Screen. Runs its own while loop until the player hits enter to continue"""
+
+    # Fills the screen with WHITE
     window.fill(COLORS['WHITE'])
+
+    # Stores a rect representing the entire window. This is used for relative placement of surfaces
     window_rect = window.get_rect()
-
-
 
     title = FONTS['TITLE'].render('Grim Ranger', True, COLORS['BLACK'])
     title_rect = title.get_rect()
@@ -23,41 +26,47 @@ def titleScreen(window, fps_clock):
     continue_prompt_rect = continue_prompt.get_rect()
     continue_prompt_rect.center = (window_rect.centerx, window_rect.height*(2/3))
 
-    continue_cover = pygame.Surface((continue_prompt_rect.width,continue_prompt_rect.height))
+    # Cover used to give the illusion of transparency to the Font surface
+    continue_cover = pygame.Surface((continue_prompt_rect.width, continue_prompt_rect.height))
     continue_cover.fill(COLORS['WHITE'])
-    continue_cover.set_alpha(200)
 
+    # Sets variables for the alpha of the cover
+    alpha_max = 200
+    alpha_min = 25
+    alpha_change_rate = 5
 
+    # Sets the alpha, stores it in cover_alpha, and sets decrease_alpha to True
+    continue_cover.set_alpha(alpha_max)
+    cover_alpha = continue_cover.get_alpha()
+    decrease_alpha = True
+
+    # Blits each of the Font surfaces to the screen
     window.blit(title, title_rect)
     window.blit(subtitle, subtitle_rect)
     window.blit(continue_prompt, continue_prompt_rect)
 
-
-
+    # Runs loop while show_title is True
     show_title = True
-
-    continue_alpha = continue_cover.get_alpha()
-    decrease_alpha = True
-
-    #continue_prompt = continue_prompt.convert()
-
     while show_title:
         checkForQuit()
         for event in pygame.event.get(KEYDOWN):
             if event.key == K_RETURN:
                 show_title = False
 
+        # Decreases or increases alpha of cover
         if decrease_alpha:
-            continue_cover.set_alpha(continue_alpha-5)
+            continue_cover.set_alpha(cover_alpha - alpha_change_rate)
         else:
-            continue_cover.set_alpha(continue_alpha+5)
+            continue_cover.set_alpha(cover_alpha + alpha_change_rate)
 
-        continue_alpha = continue_cover.get_alpha()
-        if continue_alpha == 25:
+        # Checks if alpha has reached minimum or maximum
+        cover_alpha = continue_cover.get_alpha()
+        if cover_alpha == alpha_min:
             decrease_alpha = False
-        elif continue_alpha == 200:
+        elif cover_alpha == alpha_max:
             decrease_alpha = True
 
+        # Blits continue_prompt, then cover over it
         window.blit(continue_prompt, continue_prompt_rect)
         window.blit(continue_cover, continue_prompt_rect)
 
