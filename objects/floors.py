196d18f62a19d28b15975e3fcd43a831e3f11785
hayden.foley <hpfoley1@gmail.com>
2019-07-12 21:41:40 -0400

    Added generateDungeon class method to Floor

--- objects/floors.py
+++ objects/floors.py
@@ -9,6 +9,8 @@ from scripts.constants import CELL_WIDTH, CELL_HEIGHT
 
 
 class Floor:
+    MAP_WIDTH = 39
+    MAP_HEIGHT = 22
     def __init__(self, width, height):
         self.width = width
         self.height = height
@@ -39,15 +41,44 @@ class Floor:
                 self.tile_map[xtile][ytile].update(self.map)
 
     def draw(self, surface):
+        """Draws all of the tiles in the tile map at the appropriate location
+
+        Parameters:
+            surface : pygame.Surface
+                surface to draw to
+        """
         for xtile in range(self.width):
             for ytile in range(self.height):
                 self.tile_map[xtile][ytile].draw(surface)
 
     def addEntity(self, entity):
+        """Adds an entity to the entities list attribute
+
+        Parameters:
+            entity : Entity
+                entity to add
+        """
         self.entities.append(entity)
 
     def removeEntity(self, entity):
+        """Removes an entity from the entities list attribute"""
         self.entities.remove(entity)
+
+    @classmethod
+    def generateDungeon(cls, floors):
+        """Returns a list of a specified number of floors
+
+        Parameters:
+            floors : int
+                number of floors in the dungeon
+        """
+        floor_list = list()
+
+        for index in range(floors):
+            floor_list.append(Floor(cls.MAP_WIDTH, cls.MAP_HEIGHT))
+
+        return floor_list
+
 
 
 class Tile:
