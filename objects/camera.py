58427c58bd55a14c25d7dee3f580a840534de833
Squidroot2 <hpfoley1@gmail.com>
2019-07-18 06:45:32 -0400

    Added Heigh and Width to Camera

--- objects/camera.py
+++ objects/camera.py
@@ -1,5 +1,7 @@
 #todo write camera component class
 class Camera:
+    CAMERA_WIDTH = 15
+    CAMERA_HEIGHT = 15
     """component class for Player"""
     def __init__(self, owner):
         self.owner = owner
