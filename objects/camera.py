3d18d859143511e197ef467036368ee4e44b08a6
Foley <hayden.foley@gd-ms.com>
2019-07-18 15:54:51 -0400

    Added update and getRect methods to camera

--- objects/camera.py
+++ objects/camera.py
@@ -1,7 +1,34 @@
 #todo write camera component class
+from scripts.constants import CELL_SIZE
+import pygame
+
 class Camera:
-    CAMERA_WIDTH = 15
-    CAMERA_HEIGHT = 15
-    """component class for Player"""
+    """Used for display the part of the game surface centered on a particular entity such as the player"""
+    TILE_WIDTH = 15
+    TILE_HEIGHT = 15
+    
     def __init__(self, owner):
         self.owner = owner
+        self.rect = self.getRect()
+        self.pixel_width = self.TILE_WIDTH*CELL_SIZE
+        self.pixel_height = self.TILE_WIDHT*CELL_SIZE
+        self.pixel_center = (self.owner.x*CELL_SIZE, self.owner.y*CELL_SIZE)
+    
+    def update(self):
+        """Updates the center of the camera based on the location of the camera"""
+        self.pixel_center = (self.owner.x*CELL_SIZE, self.owner.y*CELL_SIZE)
+    
+    def getRect(self):
+        """Retruns the rectanlge in pixel dimensions representing the camera
+        
+        Returns:
+            rect : pygame.Rect
+        """
+        rect = pygame.Rect(0,0,self.pixel_width,self.pixel_height)
+        rect.center = self.pixel_center
+        return rect
+        
+        
+        
+        
+    
