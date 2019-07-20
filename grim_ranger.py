a4657a21c85841c6cbc1f84c224681c666848711
Squidroot <hpfoley1@gmail.com>
2019-07-19 22:09:52 -0400

    Dungeon is generated. Player starts on up portal

--- grim_ranger.py
+++ grim_ranger.py
@@ -26,9 +26,19 @@ def setupGame(window, fps_clock):
 
     titleScreen(window, fps_clock)
     name = playerCreateScreen(window, fps_clock)
-    floor1 = Floor()
-    player = Player(name,"Officer",floor1,0,0)
-    game = Game([floor1],player)
+
+    # Generate a dungeon with a specified number of floors
+    dungeon = Floor.generateDungeon(num_of_floors=5)
+
+    # Player starts on the first floor at th location of the up portal
+    player_start_x = dungeon[0].portals['up'].x
+    player_start_y = dungeon[0].portals['up'].y
+
+    # Generates player
+    player = Player(name,"Officer",dungeon[0],player_start_x,player_start_y)
+
+    # Creates game from player and dungeon
+    game = Game(dungeon,player)
 
     return game
 
